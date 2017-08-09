f5a11123ed26fd696ac8453ed717be59349a1d1a
beer85314 <beer85314@gmail.com>
2017-08-09 04:05:10 -0400

    first commit

---
+++ show/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+
+# -*- coding: UTF-8 -*-
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.shortcuts import render_to_response
+
+def index(request):
+        friends = ['tama', 'koma', 'pochi', 'ben', 'tora']
+        return render_to_response('index.html', {'friends':friends})
+
+def tama(request):
+        return render_to_response('tama.html')
